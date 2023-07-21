6a778172ddfc87b5266913964b3a17ee3e0aedda
Cassidy <cassidymcmichael617@gmail.com>
2023-07-21 15:13:31 -0400

    Update calculator logic

--- age-calculator-app-main/src/App.jsx
+++ age-calculator-app-main/src/App.jsx
@@ -1,80 +1,76 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   const [dayInput, setDayInput] = useState(0);
   const [monthInput, setMonthInput] = useState(0);
   const [yearInput, setYearInput] = useState(0);
-  const [fullBirthDate, setFullBirthDate] = useState("");
 
   const [calculatedDays, setCalculatedDays] = useState(0);
   const [calculatedMonths, setCalculatedMonths] = useState(0);
   const [calculatedYears, setCalculatedYears] = useState(0);
+  const calculateAge = (dayInput, monthInput, yearInput) => {
 
-  const calculateAge = () => {
-    setFullBirthDate(`${monthInput}/${dayInput}/${yearInput}`);
+    let bDay = dayInput;
+    bDay = parseInt(bDay);
+
+    let bMonth = monthInput;
+    bMonth = parseInt(bMonth);
+
+    let bYear = yearInput;
+    bYear = parseInt(bYear);
+
+
     const todayFull = new Date();
-    const todayDay = todayFull.getDate();
-    const todayMonth = todayFull.getMonth() + 1; ///added + 1 bc getMonth uses a zero-based index so Jan starts at 0
-    const todayYear = todayFull.getFullYear();
+    let tDay = todayFull.getDate();
+    let tMonth = todayFull.getMonth() + 1; ///added + 1 bc getMonth uses a zero-based index so Jan starts at 0
+    let tYear = todayFull.getFullYear();
 
-    const birthDay = (new Date(fullBirthDate)).getDate();
-    const birthMonth = (new Date(fullBirthDate)).getMonth() + 1;
-    const birthYear = (new Date(fullBirthDate)).getFullYear();
-
-    let month = todayMonth;
-    let year = todayYear;
-
-    if (todayDay < birthDay) {
-      setCalculatedDays((todayDay - birthDay) + 30);
-      month -= 1;
+    if (tDay < bDay) {
+      setCalculatedDays((tDay - bDay) + 30);
+      tMonth -= 1;
     } else {
-      setCalculatedDays(todayDay - birthDay);
+      setCalculatedDays(tDay - bDay);
     }
 
-    if (month < birthMonth) {
-      setCalculatedMonths((month - birthMonth) + 12);
-      year -= 1;
+    if (tMonth < bMonth) {
+      setCalculatedMonths((tMonth - bMonth) + 12);
+      tYear -= 1;
     } else {
-      setCalculatedMonths(month - birthMonth)
+      setCalculatedMonths(tMonth - bMonth)
     }
 
-    setCalculatedYears(year - birthYear);
-
-    console.log(`${calculatedYears} years, ${calculatedMonths} months, ${calculatedDays} days`)
-    console.log(typeof calculatedYears)
-
+    setCalculatedYears(tYear - bYear);
   }
 
-  const validateForm = () => {
-    
-  }
+  // const validateForm = (event) => {
+  //   event.preventDefault();
+  //   calculateAge();
+  // }
 
-//input borders and labels must turn red if invalid. also add message "this field is required" if field is empty
-
+  //input borders and labels must turn red if invalid. also add message "this field is required" if field is empty
   return (
     <>
       <div className='card'>
         <div className='input-row'>
-          <form  className='form' name="calculteAgeForm" action="" method="" onSubmit={validateForm}>
-            <div className='col col-left'>
-              <label htmlFor='day'>day</label><br />
-              <input type='text' id="day" onChange={(e) => { setDayInput(e.target.value) }} />
-            </div>
-            <div className='col col-mid'>
-              <label htmlFor='month'>month</label><br />
-              <input type='text' id="month" onChange={(e) => { setMonthInput(e.target.value) }} />
-            </div>
-            <div className='col col-right'>
-              <label htmlFor='year'>year</label><br />
-              <input type='number' id="year" onChange={(e) => { setYearInput(e.target.value) }} />
-            </div>
-          </form>
+          {/* <form  className='form' name="calculteAgeForm" action="" method="" onSubmit={validateForm}> */}
+          <div className='col col-left'>
+            <label htmlFor='day'>day</label><br />
+            <input type='number' id="day" onChange={(e) => { setDayInput(e.target.value) }} />
+          </div>
+          <div className='col col-mid'>
+            <label htmlFor='month'>month</label><br />
+            <input type='number' id="month" onChange={(e) => { setMonthInput(e.target.value) }} />
+          </div>
+          <div className='col col-right'>
+            <label htmlFor='year'>year</label><br />
+            <input type='number' id="year" onChange={(e) => { setYearInput(e.target.value) }} />
+          </div>
+          {/* <input type='submit'/> */}
+          {/* </form> */}
         </div>
 
-        {/* <button type='button' onClick={calculateAge}>Submit</button> */}
+        <button type='button' onClick={(e) => { calculateAge(dayInput, monthInput, yearInput) }}>Submit</button>
 
         <div className='divider'>
 
@@ -82,27 +78,37 @@ function App() {
 
         <div className='result'>
           <p className='result-text'>
-            {/* {typeof calculatedYears} <br/>
-            {calculatedYears} */}
-            {/*^gives NaN on first button click which throws error for span */}
-
             <i>
-              <span className='result-num'>{calculatedYears ? {calculatedYears} : (<>--</>)}</span> 
+              {calculatedYears ? (
+                <span className='result-num'>{calculatedYears}</span>
+                ) : (
+                  <span className='result-num'>--</span>
+                )
+              }
               years
             </i> <br />
             <i>
-            <span className='result-num'>{calculatedMonths ? {calculatedMonths} : (<>--</>)}</span> 
+              {calculatedMonths ? (
+                <span className='result-num'>{calculatedMonths}</span>
+                ) : (
+                  <span className='result-num'>--</span>
+                )
+              }
               months
-            </i> <br /> 
-             <i>
-            <span className='result-num'>{calculatedDays ? {calculatedDays} : (<>--</>)}</span> 
+            </i> <br />
+            <i>
+              {calculatedDays ? (
+                <span className='result-num'>{calculatedDays}</span>
+                ) : (
+                  <span className='result-num'>--</span>
+                )
+              }
               days
-            </i>
+            </i> <br />
           </p>
         </div>
       </div>
     </>
   )
 }
-
 export default App
